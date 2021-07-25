52f9dc73a4ddfd581735b5ff5bba36e9ee61e5fd
andre-vidal <avidal@realdecoy.com>
2021-07-25 18:00:24 +0000

    export component api descriptions

---
+++ docs/pages/components/index.ts
@@ -0,0 +1,83 @@
+const autocomplete = require('./autocomplete/api/autocomplete.js');
+const button = require('./button/api/button.js');
+const carousel = require('./carousel/api/carousel.js');
+const checkbox = require('./checkbox/api/checkbox.js');
+const clockpicker = require('./clockpicker/api/clockpicker.js');
+const collapse = require('./collapse/api/collapse.js');
+const datepicker = require('./datepicker/api/datepicker.js');
+const datetimepicker = require('./datetimepicker/api/datetimepicker.js');
+const dialog = require('./dialog/api/dialog.js');
+const dropdown = require('./dropdown/api/dropdown.js');
+const field = require('./field/api/field.js');
+const icon = require('./icon/api/icon.js');
+const image = require('./image/api/image.js');
+const input = require('./input/api/input.js');
+const loading = require('./loading/api/loading.js');
+const menu = require('./menu/api/menu.js');
+const message = require('./message/api/message.js');
+const modal = require('./modal/api/modal.js');
+const navbar = require('./navbar/api/navbar.js');
+const notification = require('./notification/api/notification.js');
+const numberinput = require('./numberinput/api/numberinput.js');
+const pagination = require('./pagination/api/pagination.js');
+const progress = require('./progress/api/progress.js');
+const radio = require('./radio/api/radio.js');
+const rate = require('./rate/api/rate.js');
+const select = require('./select/api/select.js');
+const sidebar = require('./sidebar/api/sidebar.js');
+const skeleton = require('./skeleton/api/skeleton.js');
+const slider = require('./slider/api/slider.js');
+const snackbar = require('./snackbar/api/snackbar.js');
+const steps = require('./steps/api/steps.js');
+const switchComponent = require('./switch/api/switch.js');
+const table = require('./table/api/table.js');
+const tabs = require('./tabs/api/tabs.js');
+const tag = require('./tag/api/tag.js');
+const taginput = require('./taginput/api/taginput.js');
+const timepicker = require('./timepicker/api/timepicker.js');
+const toast = require('./toast/api/toast.js');
+const tooltip = require('./tooltip/api/tooltip.js');
+const upload = require('./upload/api/upload.js');
+
+export default {
+  autocomplete,
+  button,
+  carousel,
+  checkbox,
+  clockpicker,
+  collapse,
+  datepicker,
+  datetimepicker,
+  dialog,
+  dropdown,
+  field,
+  icon,
+  image,
+  input,
+  loading,
+  menu,
+  message,
+  modal,
+  navbar,
+  notification,
+  numberinput,
+  pagination,
+  progress,
+  radio,
+  rate,
+  select,
+  sidebar,
+  skeleton,
+  slider,
+  snackbar,
+  steps,
+  switchComponent,
+  table,
+  tabs,
+  tag,
+  taginput,
+  timepicker,
+  toast,
+  tooltip,
+  upload,
+}
